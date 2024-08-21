7d7da4df8a056707013c5984dc8c53daf39d1e61
alankang30 <alankang30@gmail.com>
2024-08-21 18:08:28 -0400

    dev: header, hero, features

--- src/app/(landing)/page.tsx
+++ src/app/(landing)/page.tsx
@@ -1,22 +1,83 @@
 import { Button } from "@/components/ui/button"
+import {
+  NavigationMenu,
+  NavigationMenuContent,
+  NavigationMenuIndicator,
+  NavigationMenuItem,
+  NavigationMenuLink,
+  NavigationMenuList,
+  NavigationMenuTrigger,
+  NavigationMenuViewport,
+} from "@/components/ui/navigation-menu"
+import {
+  Accordion,
+  AccordionContent,
+  AccordionItem,
+  AccordionTrigger,
+} from "@/components/ui/accordion"
+
+import { navigationMenuTriggerStyle } from "@/components/ui/navigation-menu"
+import Link from "next/link"
+
+
 
 export default function Landing() {
   return (
     <main className='flex min-h-screen flex-col items-center justify-between p-24'>
       {/* Navbar */}
-      <div className='flex flex-row items-center justify-left w-full p-4'>
-        <Button variant={"ghost"} className="m-4">Home</Button>
-        <Button variant={"ghost"} className="m-4">This button</Button>
-        <Button variant={"ghost"} className="m-4">Hello</Button>
-        <Button variant={"ghost"} className="m-4">Rate a professor?</Button>
-      </div>
+      <NavigationMenu>
+        <NavigationMenuList>
+          <NavigationMenuItem>
+            <NavigationMenuTrigger>can put links here</NavigationMenuTrigger>
+            <NavigationMenuContent>
+              <NavigationMenuLink>link is not cooperating</NavigationMenuLink>
+            </NavigationMenuContent>
+          </NavigationMenuItem>
+          <NavigationMenuItem>
+            <Link href="/docs" legacyBehavior passHref>
+              <NavigationMenuLink className={navigationMenuTriggerStyle()}>
+                professors
+              </NavigationMenuLink>
+            </Link>
+          </NavigationMenuItem>
+          <NavigationMenuItem>
+            <Link href="/docs" legacyBehavior passHref>
+              <NavigationMenuLink className={navigationMenuTriggerStyle()}>
+                Something else here
+              </NavigationMenuLink>
+            </Link>
+          </NavigationMenuItem>
+          <NavigationMenuItem>
+            <Link href="/docs" legacyBehavior passHref>
+              <NavigationMenuLink className={navigationMenuTriggerStyle()}>
+                Documentation
+              </NavigationMenuLink>
+            </Link>
+          </NavigationMenuItem>
+        </NavigationMenuList>
+      </NavigationMenu>
+
       {/* Hero Section */}
-      <div className='flex flex-col items-center justify-center w-full h-80% p-8 border'>
-        hero
-        <p>Lorem ipsem and whatever and hero ahweriowafjsdlifjsdlkfjasldfj</p>
-        <p>Lorem ipsem and whatever and hero ahweriowafjsdlifjsdlkfjasldfj</p>
+      <div className='flex flex-row items-center justify-center w-full h-80% p-8 border'>
+        <p>hero section. Im not really sure what you want in this part</p>
+        {/* put an image here */}
       </div>
       {/* Features */}
+      <Accordion type="single" collapsible className="w-80">
+        <AccordionItem value="item-1">
+          <AccordionTrigger>Does it have cool features?</AccordionTrigger>
+          <AccordionContent>
+            Yes. It adheres to the WAI-ARIA design pattern.
+          </AccordionContent>
+        </AccordionItem>
+        <AccordionItem value="item-2">
+          <AccordionTrigger>Look at those professors!</AccordionTrigger>
+          <AccordionContent>
+            wow look at them.
+          </AccordionContent>
+        </AccordionItem>
+      </Accordion>
+
       {/* Footer */}
     </main>
   );
